32aa093d3a11ae2ab60363107645e0e33fe595d4
PeterFirsov <pvfirsov15@yandex.ru>
2019-07-13 19:03:40 +0300

    убрал лишние переменные

--- js/data.js
+++ js/data.js
@@ -1,12 +1,7 @@
 'use strict';
 
 (function () {
-  var map = document.querySelector('.map');
-  var type = ['palace', 'flat', 'house', 'bungalo'];
-  var yLocation = {
-    min: 130,
-    max: 630
-  };
+
   var OBJECT_COUNT = 8;
 
   window.objects = [];
